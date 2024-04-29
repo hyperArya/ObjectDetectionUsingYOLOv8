cd1cdfa4838d18b8f2b9d7de751b1e5d3c295872
Arshdeep Yadav <112233902+hyperArya@users.noreply.github.com>
2024-04-29 21:37:02 +0530

    Update realtimeObjectDetectionUsing CV2.py

--- realtimeObjectDetectionUsing CV2.py
+++ realtimeObjectDetectionUsing CV2.py
@@ -1,8 +1,9 @@
+#use 'pip install ultralytics' to install the required libraries
 from ultralytics import YOLO
 
 import cv2
 
-mode = YOLO(r"C:\Users\arshd\Downloads\best.onnx")
+mode = YOLO(r"C:\Users\arshd\Downloads\best.onnx") #download the best.onnx or best.pt and change the file path
 
 result = mode.predict (source="0", show=True)
 print(result)
