520097acfac60f744ab3dcff8376d8cbd49ce33b
Arshdeep Yadav <112233902+hyperArya@users.noreply.github.com>
2024-04-29 21:45:27 +0530

    Update realtimeObjectDetectionUsing CV2.py

--- realtimeObjectDetectionUsing CV2.py
+++ realtimeObjectDetectionUsing CV2.py
@@ -1,9 +1,12 @@
+#this code will not run in kaggle and google colab notebook because they don't support cv
+#jupyter notebook recommended
 #use 'pip install ultralytics' to install the required libraries
+#after installing all the required libraries Restart Kernel
 from ultralytics import YOLO
-
 import cv2
 
-mode = YOLO(r"C:\Users\arshd\Downloads\best.onnx") #download the best.onnx or best.pt and change the file path
+#download the best.onnx or best.pt and change the file path
+model = YOLO(r"C:\Users\arshd\Downloads\best.onnx")
 
-result = mode.predict (source="0", show=True)
+result = model.predict (source="0", show=True)
 print(result)
